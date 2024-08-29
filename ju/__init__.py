b0b76fc153967a5dd3444b84531ebdb346c3d652
Thor Whalen <thorwhalen1@gmail.com>
2024-08-29 16:37:22 +0100

    fix: import name

--- ju/__init__.py
+++ ju/__init__.py
@@ -4,4 +4,9 @@ from ju.oas import Route, Routes
 from ju.rjsf import func_to_form_spec
 from ju.json_schema import function_to_json_schema, json_schema_to_signature
 from ju.util import truncate_dict_values
-from ju.pydantic_util import is_valid_wrt_model, valid_models, create_pydantic_model
+from ju.pydantic_util import (
+    is_valid_wrt_model,
+    valid_models,
+    data_to_pydantic_model,
+    pydantic_model_to_code,
+)
